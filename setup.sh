fabc1f83c0cf865cf095809cba923b8f642f5f6d
sammy <142356200+SamaraCarreiro@users.noreply.github.com>
2025-02-23 12:42:02 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-sudo su
-yum -y install httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
+
+yum update -y
+yum -y install httpd git
+
+systemctl start httpd
+systemctl enable httpd
+
 git clone https://github.com/SamaraCarreiro/atividadeAWS.git /var/www/html/
+
 chown -R apache:apache /var/www/html
 chmod -R 755 /var/www/html
-sudo systemctl restart httpd
-curl -X POST -d "nome-Samara Carreiro" https://difusaotech.com.br/lab/aws/index.php 
+
+systemctl restart httpd
+
+systemctl status httpd >> /var/log/setup.log 2>&1
+
+curl -X POST -d "nome-Samara Carreiro" https://difusaotech.com.br/lab/aws/index.php >> /var/log/setup.log 2>&1
+
 echo "Script concluído com sucesso!" >> /var/log/setup.log
